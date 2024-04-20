0f0067d8f5f50dcf39dd616d38669dfe2e2c448f
ssssu <cdw3521@naver.com>
2024-04-20 15:36:31 +0900

    🐻인사말이 번갈아가며 출력되도록 코드 작성 -> toggle 사용

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ window.addEventListener("scroll", function () {
     header.classList.remove("fixed");
   }
 });
+
+// 5초마다 2개의 인사말을 바꿔가며 보여주기 위해 toggle 사용
+const produce = document.querySelectorAll(".produce");
+setInterval(() => {
+  for (let i = 0; i < 2; i++) {
+    produce[i].classList.toggle("active");
+  }
+}, 5000);
